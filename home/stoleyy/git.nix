1532798a7c0315dff98dca88413ff2022edf3364
stoleyy <snapscanned@proton.me>
2026-05-12 23:43:28 +0000

    git.nix: use programs.git.settings as HM 25.11 expects

--- home/stoleyy/git.nix
+++ home/stoleyy/git.nix
@@ -2,15 +2,15 @@
 
 {
   programs.git = {
-    enable    = true;
-    userName  = "stoleyy";
-    userEmail = "snapscanned@proton.me";
-    aliases = {
-      s    = "status -sb";
-      lg   = "log --oneline --graph --decorate --all";
-      last = "log -1 HEAD --stat";
-    };
-    extraConfig = {
+    enable = true;
+    settings = {
+      user.name  = "stoleyy";
+      user.email = "snapscanned@proton.me";
+      alias = {
+        s    = "status -sb";
+        lg   = "log --oneline --graph --decorate --all";
+        last = "log -1 HEAD --stat";
+      };
       init.defaultBranch   = "main";
       pull.rebase          = true;
       push.autoSetupRemote = true;
